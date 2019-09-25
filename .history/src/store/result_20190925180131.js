229e79f462ba8dae3014d343073b99b4bc11a106
Liujinghhh <1944465209@qq.com>
2019-09-25 21:07:31 +0800

    cry :too bug

---
+++ .history/src/store/result_20190925180131.js
@@ -0,0 +1,71 @@
+import {
+    SET_FIRST,
+    SET_SECOND,
+    SET_THIRD,
+    SET_FOURTH
+} from './type/mutations'
+
+import {
+
+
+} from './type/actions'
+
+import {
+    ResultService,
+
+} from '../common/service/api.js'
+
+
+
+const initialState = {
+    first: unlock,
+    second: lock,
+    third: lock,
+    forth: lock
+
+
+
+
+
+}
+
+const state = { ...initialState }
+
+const mutations = {
+
+    [](state) {
+        state.questionIndex -= 1;
+        let index = state.questionIndex;
+        let result = state.choosedList[index]
+        state.choosedNum = result - 1
+    }
+
+
+}
+
+
+const actions = {
+    async[FETCH_FIRST_ANSWER]({ commit }, params) {
+
+        let { data } = await ResultService.getFirstAnswer(params)
+        console.log(data)
+        commit(SET_FIRST_ANSWER, data)
+
+    },
+
+
+}
+
+const getters = {
+
+
+}
+
+
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
---
+++ .history/src/store/type/mutations_20190925175440.js
@@ -0,0 +1,4 @@
+export const SET_FIRST = 'setFirst'
+export const SET_SECOND = 'setSecond'
+export const SET_THIRD = 'setThird'
+export const SET_FOURTH = 'setForth'
