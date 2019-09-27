ced0c3a6a6f00a1017cd0a9a87849f53c5ff2367
zljkarry <1710160855@qq.com>
2019-09-27 17:56:45 +0800

    delete .history

--- .history/src/store/type/actions_20190925175952.js
+++
@@ -1,4 +0,0 @@
-export const FETCH_FIRST = 'fetchFirst'
-export const FETCH_SECOND = 'fetchSecond'
-export const FETCH_THIRD = 'fetchThird'
-export const FETCH_FOURTH = 'fetchForth'
--- .history/src/store/type/mutations_20190925175424.js
+++
@@ -1,4 +0,0 @@
-export const SET_FIRST = 'setFirst'
-export const SET_SECOND = 'setSecond'
-export const SET_THIRD = 'setThird'
-export const SET_FOURTH = 'set'
